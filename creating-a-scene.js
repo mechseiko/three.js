abfc53018dba5a85e003dbb13ef94b8025880601
mechseiko <mechseiko@gmail.com>
2025-05-29 17:47:59 +0100

    loaders

--- creating-a-scene.js
+++ creating-a-scene.js
@@ -14,7 +14,6 @@ import * as THREE from 'three';
 // const controls = new OrbitControls(camera, renderer.domElement);
 // const loader = new GLTFLoader();
 
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
